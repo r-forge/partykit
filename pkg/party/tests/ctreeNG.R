107b4730a78bfb51b5621118579566a55fd540bd
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2016-08-18 12:48:17 +0000

    add partykit::ctree
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@904 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/party/tests/ctreeNG.R
+++ pkg/party/tests/ctreeNG.R
@@ -5,5 +5,7 @@ data("iris")
 
 ctree(Species ~ ., data = iris, control = ctree_control(nmax = Inf))
 
+partykit::ctree(Species ~ ., data = iris)
+
 ctree(Species ~ ., data = iris, control = ctree_control(nmax = 50))
 
