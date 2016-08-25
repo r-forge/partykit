444b656d42b9775970597ec60a6539b99b61f484
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2016-08-25 12:28:13 +0000

    * not :
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@929 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/party/tests/glmtree.R
+++ pkg/party/tests/glmtree.R
@@ -13,7 +13,7 @@ pid_tree <- partykit::glmtree(diabetes ~ glucose | pregnant +
 pid_tree
 
 nd <- factor(predict(pid_tree, type = "node"))
-m <- glm(diabetes ~ glucose:nd, data = PimaIndiansDiabetes, family = binomial)
+m <- glm(diabetes ~ glucose*nd, data = PimaIndiansDiabetes, family = binomial)
 logLik(m)
 AIC(m)
 
@@ -26,7 +26,7 @@ pid_tree1
 info_node(node_party(pid_tree1))
 
 nd1 <- factor(predict(pid_tree1, type = "node"))
-m1 <- glm(diabetes ~ glucose:nd1, data = PimaIndiansDiabetes, family = binomial)
+m1 <- glm(diabetes ~ glucose*nd1, data = PimaIndiansDiabetes, family = binomial)
 logLik(m1)
 AIC(m1)
 
@@ -43,7 +43,7 @@ pid_tree2
 info_node(node_party(pid_tree2))
 
 nd2 <- factor(predict(pid_tree2, type = "node"))
-m2 <- glm(diabetes ~ glucose:nd2, data = PimaIndiansDiabetes, family = binomial)
+m2 <- glm(diabetes ~ glucose*nd2, data = PimaIndiansDiabetes, family = binomial)
 logLik(m2)
 AIC(m2)
 
