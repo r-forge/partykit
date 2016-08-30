9259fcde600cd5af3c40b2c28ad0fef7a300a0b1
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2016-08-30 10:13:23 +0000

    add test for splittest
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@948 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/party/tests/ctreeNG.R
+++ pkg/party/tests/ctreeNG.R
@@ -5,7 +5,15 @@ data("iris")
 
 ctree(Species ~ ., data = iris, control = ctree_control(nmax = Inf))
 
+ctree(Species ~ ., data = iris, control = ctree_control(nmax = Inf,
+      splittest = TRUE, nresample = 1000, testtype = "MonteCarlo"))
+
+
+
 partykit::ctree(Species ~ ., data = iris)
 
 ctree(Species ~ ., data = iris, control = ctree_control(nmax = 50))
+
+ctree(Species ~ ., data = iris, control = ctree_control(nmax = 50, splittest = TRUE,
+                     nresample = 1000, testtype = "MonteCarlo"))
 
