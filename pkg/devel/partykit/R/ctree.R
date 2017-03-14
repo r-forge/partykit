c89a3d9726dc5ce00dc8afeca1ac5348fe28e385
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2017-03-14 14:42:51 +0000

    check for nmax
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@1158 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/devel/partykit/R/ctree.R
+++ pkg/devel/partykit/R/ctree.R
@@ -252,7 +252,7 @@
             if (!is.null(sc)) attr(mf[[nm]], "scores") <- sc
         }
         y <- model.part(f, data = mf, lhs = 1, rhs = 0)
-        if (!is.factor(y))
+        if ((ncol(y) > 1L) || !is.factor(y[[1]]))
             warning("nmax < Inf for non-categorical response not recommended")
         bdr <- inum::inum(y, nmax = ctrl$nmax, total = TRUE, 
                           complete.cases.only = TRUE)
