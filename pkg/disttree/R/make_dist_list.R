91bf114e627152a1efbbc65a20eae84df4f5a7ee
lschlosser (Lisa Schlosser) <Lisa.Schlosser@uibk.ac.at>
2017-04-04 10:18:20 +0000

    the function weight_mean_expression now also works for censored data; bugfixes in make_censored_dist_list: now the correct d-,s- and h-functions from crch are applied and the censoring points are handed over correctly
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@1207 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/disttree/R/make_dist_list.R
+++ pkg/disttree/R/make_dist_list.R
@@ -79,8 +79,7 @@ make_dist_list <- function(family, bd = NULL)
           input <- c(input, par[par.id])
           input$bd <- bd
           val <- do.call(fun, input)
-          ny <- length(y)
-          if(survival::is.Surv(y)) ny <- dim(y)[1]
+          ny <- if(survival::is.Surv(y)) dim(y)[1] else length(y)
           if(length(val) == 1L) val <- rep(val, ny)
           return(val)
         }
@@ -90,8 +89,7 @@ make_dist_list <- function(family, bd = NULL)
           input$y <- y
           input <- c(input, par[par.id])
           val <- do.call(fun, input)
-          ny <- length(y)
-          if(survival::is.Surv(y)) ny <- dim(y)[1]
+          ny <- if(survival::is.Surv(y)) dim(y)[1] else length(y)
           if(length(val) == 1L) val <- rep(val, ny)
           return(val)
         }    
@@ -102,16 +100,14 @@ make_dist_list <- function(family, bd = NULL)
           input <- list()
           input <- c(input, par[par.id])
           input$bd <- bd
-          ny <- length(y)
-          if(survival::is.Surv(y)) ny <- dim(y)[1]
+          ny <- if(survival::is.Surv(y)) dim(y)[1] else length(y)
           return(rep(do.call(fun, input), ny))
         }
       } else {
         derivfun <- function(y, par) {
           input <- list()
           input <- c(input, par[par.id])
-          ny <- length(y)
-          if(survival::is.Surv(y)) ny <- dim(y)[1]
+          ny <- if(survival::is.Surv(y)) dim(y)[1] else length(y)
           return(rep(do.call(fun, input), ny))
         }    
       }
@@ -227,22 +223,18 @@ make_dist_list <- function(family, bd = NULL)
   ## define startfunction, complete derivative functions dpardeta, d2pardeta2, dldpar, d2ldpar2 according to the number of parameters
   
   ## TO DO: change starting expressions: eg. mean -> weighted.mean (use gsub to substitute function names)
-  weight_mean_expression <- function(e) {
-    if(!("censored" %in% strsplit(family$family[2], split = " ")[[1]])) {
-      e <- deparse(e)
+  weight_mean_expression <- if("censored" %in% strsplit(family$family[2], split = " ")[[1]]) {
+    function(e) {
+      e <- gsub("mean(y[, 1])", "weighted.mean(y[, 1], weights)", e, fixed = TRUE)
+      e <- parse(text = e)
+      return(e)
+    }
+  } else {
+    function(e) {
       e <- gsub("mean(y)", "weighted.mean(y, weights)", e, fixed = TRUE)
       e <- parse(text = e)
-      e <- eval(e)
+      return(e)
     }
-    
-    # FIX ME: adapt for censored distribution
-    # if("censored" %in% strsplit(family$family[2], split = " ")[[1]]) {
-    #   e <- gsub("mean(y[,1])", "weighted.mean(y[,1], weights)", e, fixed = TRUE)
-    # } else {
-    #   e <- gsub("mean(y)", "weighted.mean(y, weights)", e, fixed = TRUE)
-    # }
-    
-    return(e)
   }
   
   
@@ -297,8 +289,7 @@ make_dist_list <- function(family, bd = NULL)
       # for each observation a matrix of size (1x1) is stored in d2list
       
       d2list <- list()
-      ny <- length(y)
-      if(survival::is.Surv(y)) ny <- dim(y)[1]
+      ny <- if(survival::is.Surv(y)) dim(y)[1] else length(y)
       length(d2list) <- ny
       for(i in 1:ny){
         d2list[[i]] <- d2matrix[c(i),]
@@ -386,8 +377,7 @@ make_dist_list <- function(family, bd = NULL)
       # for each observation a matrix of size (2x2) is stored in d2list
       
       d2list <- list()
-      ny <- length(y)
-      if(survival::is.Surv(y)) ny <- dim(y)[1]
+      ny <- if(survival::is.Surv(y)) dim(y)[1] else length(y)
       length(d2list) <- ny
       for(i in 1:ny){
         d2list[[i]] <- d2matrix[c(i, ny+i),]
@@ -477,8 +467,7 @@ make_dist_list <- function(family, bd = NULL)
       # for each observation a matrix of size (3x3) is stored in d2list
       
       d2list <- list()
-      ny <- length(y)
-      if(survival::is.Surv(y)) ny <- dim(y)[1]
+      ny <- if(survival::is.Surv(y)) dim(y)[1] else length(y)
       length(d2list) <- ny
       for(i in 1:ny){
         d2list[[i]] <- d2matrix[c(i, ny+i, 2*ny+i),]
@@ -570,8 +559,7 @@ make_dist_list <- function(family, bd = NULL)
       # for each observation a matrix of size (4x4) is stored in d2list
       
       d2list <- list()
-      ny <- length(y)
-      if(survival::is.Surv(y)) ny <- dim(y)[1]
+      ny <- if(survival::is.Surv(y)) dim(y)[1] else length(y)
       length(d2list) <- ny
       for(i in 1:ny){
         d2list[[i]] <- d2matrix[c(i, ny+i, 2*ny+i, 3*ny+i),]
@@ -638,7 +626,10 @@ make_dist_list <- function(family, bd = NULL)
     names(input) <- inputnames
     eval <- do.call(get(paste0("d", family$family[[1]])), input)
     if(sum) {
-      if(is.null(weights)) weights <- rep.int(1, length(y))
+      if(is.null(weights)) {
+        ny <- if(survival::is.Surv(y)) dim(y)[1] else length(y)
+        weights <- rep.int(1, ny)
+      }
       eval <- sum(weights * eval)
     }
     return(eval)
@@ -664,7 +655,8 @@ make_dist_list <- function(family, bd = NULL)
     score <- as.matrix(score)
     colnames(score) <- etanames
     if(sum) {
-      if(is.null(weights)) weights <- rep.int(1, length(y))
+      ny <- if(survival::is.Surv(y)) dim(y)[1] else length(y)
+      if(is.null(weights)) weights <- rep.int(1, ny)
       score <- colSums(weights * score)
     }
     return(score)
@@ -673,8 +665,7 @@ make_dist_list <- function(family, bd = NULL)
   
   ## hessian (second-order partial derivatives of the (positive) log-likelihood function)
   hdist <- function(y, eta, weights = NULL) {    
-    ny <- length(y)
-    if(survival::is.Surv(y)) ny <- dim(y)[1]
+    ny <- if(survival::is.Surv(y)) dim(y)[1] else length(y)
     if(is.null(weights)) weights <- rep.int(1, ny)
     
     par <- linkinv(eta)
@@ -871,14 +862,14 @@ if(FALSE) {
 
 
 ###### function that returns dist_list for censored distributions
-make_censored_dist_list <- function(distribution = c("normal","logistic"), 
+make_censored_dist_list <- function(dist = c("normal","logistic"), 
                                      type = c("left", "right", "interval"),
                                      censpoint = 0)
 {
   if((type == "interval") && !(length(censpoint) == 2)) stop("for type 'intervall' two censoring points have to be set")
   if((type %in% c("left", "right")) && !(length(censpoint) ==1)) stop("for type 'left' or 'right' one censoring point has to be set")
   
-  family.name <- if(distribution == "normal") "censored Normal Distribution" else "censored Logistic Distribution"
+  family.name <- if(dist == "normal") "censored Normal Distribution" else "censored Logistic Distribution"
   
   if(type == "interval") {
     list.name <- paste("dist_list_cens", type, censpoint[1], censpoint[2], sep = "_")
@@ -902,7 +893,7 @@ make_censored_dist_list <- function(distribution = c("normal","logistic"),
   parnames <- c("mu", "sigma")
   etanames <- c("mu", "log(sigma)")
   
-  ddist <-  function(y, eta, log = TRUE, weights = NULL, sum = FALSE, left = left, right = right) {     
+  ddist <-  function(y, eta, log = TRUE, weights = NULL, sum = FALSE) {     
     par <- c(eta[1], exp(eta[2]))
     val <- crch::dcnorm(x = y, mean = par[1], sd = par[2], left = left, right = right, log = log)
     if(sum) {
@@ -913,7 +904,7 @@ make_censored_dist_list <- function(distribution = c("normal","logistic"),
   }
   
   
-  sdist <- function(y, eta, weights = NULL, sum = FALSE, left = left, right = right) {   
+  sdist <- function(y, eta, weights = NULL, sum = FALSE) {   
     par <- c(eta[1], exp(eta[2]))
     # y[y==0] <- 1e-323
     
@@ -933,7 +924,7 @@ make_censored_dist_list <- function(distribution = c("normal","logistic"),
   }
   
   
-  hdist <- function(y, eta, weights = NULL, left = left, right = right) {    
+  hdist <- function(y, eta, weights = NULL) {    
     ny <- length(y)
     if(is.null(weights)) weights <- rep.int(1, ny)
     
@@ -958,8 +949,65 @@ make_censored_dist_list <- function(distribution = c("normal","logistic"),
   }
   
   
-  ## additional functions pdist, qdist, rdist
-  if(distribution == "normal") {
+  
+  if(dist == "normal") {
+    
+    ddist <-  function(y, eta, log = TRUE, weights = NULL, sum = FALSE) {     
+      par <- c(eta[1], exp(eta[2]))
+      val <- crch::dcnorm(x = y, mean = par[1], sd = par[2], left = left, right = right, log = log)
+      if(sum) {
+        if(is.null(weights)) weights <- if(is.matrix(y)) rep.int(1, dim(y)[1]) else rep.int(1, length(y))
+        val <- sum(weights * val, na.rm = TRUE)
+      }
+      return(val)
+    }
+    
+    
+    sdist <- function(y, eta, weights = NULL, sum = FALSE) {   
+      par <- c(eta[1], exp(eta[2]))
+      # y[y==0] <- 1e-323
+      
+      score_m <- crch:::scnorm(x = y, mean = par[1], sd = par[2], which = "mu", left = left, right = right)
+      score_s <- crch:::scnorm(x = y, mean = par[1], sd = par[2], which = "sigma", left = left, right = right) * exp(eta[2]) # inner derivation exp(eta[2])
+      score <- cbind(score_m, score_s)
+      score <- as.matrix(score)
+      colnames(score) <- etanames
+      if(sum) {
+        if(is.null(weights)) weights <- rep.int(1, length(y)[1])
+        # if score == Inf replace score with 1.7e308 because Inf*0 would lead to NaN (0 in weights)
+        score[score==Inf] = 1.7e308
+        score <- colSums(weights * score, na.rm = TRUE)
+        #if(any(is.nan(score))) print(c(eta, "y", y))
+      }
+      return(score)
+    }
+    
+    
+    hdist <- function(y, eta, weights = NULL) {    
+      ny <- length(y)
+      if(is.null(weights)) weights <- rep.int(1, ny)
+      
+      par <- c(eta[1], exp(eta[2]))                           
+      # y[y==0] <- 1e-323
+      
+      d2mu <- crch:::hcnorm(x = y, mean = par[1], sd = par[2], which = "mu", left = left, right = right)
+      d2sigma <- crch:::hcnorm(x = y, mean = par[1], sd = par[2], which = "sigma", left = left, right = right)
+      dmudsigma <- crch:::hcnorm(x = y, mean = par[1], sd = par[2], which = "mu.sigma", left = left, right = right) # FIX: order?
+      dsigmadmu <- crch:::hcnorm(x = y, mean = par[1], sd = par[2], which = "sigma.mu", left = left, right = right) # FIX: order?
+      dsigma <- crch:::scnorm(x = y, mean = par[1], sd = par[2], which = "sigma", left = left, right = right)
+      
+      d2ld.etamu2 <- sum(weights * d2mu, na.rm = TRUE)
+      d2ld.etamu.d.etasigma <- sum(weights * dmudsigma * par[2], na.rm = TRUE)
+      d2ld.etasigma.d.etamu <- sum(weights * dsigmadmu * par[2], na.rm = TRUE)
+      d2ld.etasigma2 <- sum(weights * (d2sigma * exp(2*eta[2]) + dsigma * par[2]), na.rm = TRUE)         
+      
+      hess <- matrix(c(d2ld.etamu2, d2ld.etamu.d.etasigma, d2ld.etasigma.d.etamu, d2ld.etasigma2), nrow = 2)
+      colnames(hess) <- rownames(hess) <-  etanames
+      
+      return(hess)
+    }
+    
+    ## additional functions pdist, qdist, rdist
     pdist <- function(q, eta, lower.tail = TRUE, log.p = FALSE) crch:::pcnorm(q, mean = eta[1], sd = eta[2], 
                                                                               lower.tail = lower.tail, log.p = log.p, 
                                                                               left = left, right = right)
@@ -969,7 +1017,70 @@ make_censored_dist_list <- function(distribution = c("normal","logistic"),
     rdist <- function(n, eta) crch:::rcnorm(n, mean = eta[1], sd = eta[2], left = left, right = right)
   }
   
-  if(distribution == "logistic") {
+  
+  
+  
+  
+  
+  
+  if(dist == "logistic") {
+    
+    ddist <-  function(y, eta, log = TRUE, weights = NULL, sum = FALSE) {     
+      par <- c(eta[1], exp(eta[2]))
+      val <- crch::dclogis(x = y, mean = par[1], sd = par[2], left = left, right = right, log = log)
+      if(sum) {
+        if(is.null(weights)) weights <- if(is.matrix(y)) rep.int(1, dim(y)[1]) else rep.int(1, length(y))
+        val <- sum(weights * val, na.rm = TRUE)
+      }
+      return(val)
+    }
+    
+    
+    sdist <- function(y, eta, weights = NULL, sum = FALSE) {   
+      par <- c(eta[1], exp(eta[2]))
+      # y[y==0] <- 1e-323
+      
+      score_m <- crch:::sclogis(x = y, mean = par[1], sd = par[2], which = "mu", left = left, right = right)
+      score_s <- crch:::sclogis(x = y, mean = par[1], sd = par[2], which = "sigma", left = left, right = right) * exp(eta[2]) # inner derivation exp(eta[2])
+      score <- cbind(score_m, score_s)
+      score <- as.matrix(score)
+      colnames(score) <- etanames
+      if(sum) {
+        if(is.null(weights)) weights <- rep.int(1, length(y)[1])
+        # if score == Inf replace score with 1.7e308 because Inf*0 would lead to NaN (0 in weights)
+        score[score==Inf] = 1.7e308
+        score <- colSums(weights * score, na.rm = TRUE)
+        #if(any(is.nan(score))) print(c(eta, "y", y))
+      }
+      return(score)
+    }
+    
+    
+    hdist <- function(y, eta, weights = NULL) {    
+      ny <- length(y)
+      if(is.null(weights)) weights <- rep.int(1, ny)
+      
+      par <- c(eta[1], exp(eta[2]))                           
+      # y[y==0] <- 1e-323
+      
+      d2mu <- crch:::hclogis(x = y, mean = par[1], sd = par[2], which = "mu", left = left, right = right)
+      d2sigma <- crch:::hclogis(x = y, mean = par[1], sd = par[2], which = "sigma", left = left, right = right)
+      dmudsigma <- crch:::hclogis(x = y, mean = par[1], sd = par[2], which = "mu.sigma", left = left, right = right) # FIX: order?
+      dsigmadmu <- crch:::hclogis(x = y, mean = par[1], sd = par[2], which = "sigma.mu", left = left, right = right) # FIX: order?
+      dsigma <- crch:::sclogis(x = y, mean = par[1], sd = par[2], which = "sigma", left = left, right = right)
+      
+      d2ld.etamu2 <- sum(weights * d2mu, na.rm = TRUE)
+      d2ld.etamu.d.etasigma <- sum(weights * dmudsigma * par[2], na.rm = TRUE)
+      d2ld.etasigma.d.etamu <- sum(weights * dsigmadmu * par[2], na.rm = TRUE)
+      d2ld.etasigma2 <- sum(weights * (d2sigma * exp(2*eta[2]) + dsigma * par[2]), na.rm = TRUE)         
+      
+      hess <- matrix(c(d2ld.etamu2, d2ld.etamu.d.etasigma, d2ld.etasigma.d.etamu, d2ld.etasigma2), nrow = 2)
+      colnames(hess) <- rownames(hess) <-  etanames
+      
+      return(hess)
+    }
+    
+    ## additional functions pdist, qdist, rdist
     pdist <- function(q, eta, lower.tail = TRUE, log.p = FALSE) crch:::pclogis(q, mean = eta[1], sd = eta[2], 
                                                                               lower.tail = lower.tail, log.p = log.p, 
                                                                               left = left, right = right)
@@ -979,6 +1090,8 @@ make_censored_dist_list <- function(distribution = c("normal","logistic"),
     rdist <- function(n, eta) crch:::rclogis(n, mean = eta[1], sd = eta[2], left = left, right = right)
   }
 
+  
+  
   
   link <- c("identity", "log")
   
@@ -1009,8 +1122,8 @@ make_censored_dist_list <- function(distribution = c("normal","logistic"),
       yc[yc<censpoint[1]] <- censpoint[1]
       yc[yc>censpoint[2]] <- censpoint[2]
     }
-    if(type == "left") yc <- max(censpoint,y) 
-    if(type == "right") yc <- min(censpoint,y) 
+    if(type == "left") yc <- pmax(censpoint,y) 
+    if(type == "right") yc <- pmin(censpoint,y) 
     
     if(is.null(weights)) {
       mu <- mean(yc)
@@ -1029,6 +1142,7 @@ make_censored_dist_list <- function(distribution = c("normal","logistic"),
   dist_list <- list(family.name = family.name,
                     list.name = list.name,
                     type = type,
+                    dist = dist,
                     censpoint = censpoint,
                     ddist = ddist, 
                     sdist = sdist, 
@@ -1148,7 +1262,7 @@ if(FALSE) {
   
   
   startfun <- function(y, weights = NULL){
-    yc <- max(0,y)  # optional ?
+    yc <- pmax(0,y)  # optional ?
     if(is.null(weights)) {
       mu <- mean(yc)
       sigma <- sqrt(1/length(yc) * sum((yc - mu)^2))
@@ -1407,7 +1521,6 @@ if(FALSE) {
   }
   
   
-  # FIX ME: adapt lambda
   ## additional functions pdist, qdist, rdist
   {
     pdist <- function(q, eta, lower.tail = TRUE, log.p = FALSE) ppois(q, lambda = exp(eta[1]), lower.tail = lower.tail, log.p = log.p)
@@ -1508,8 +1621,6 @@ if(FALSE) {
   
   
   hdist <- function(y, eta, weights = NULL) {    
-    ny <- length(y)
-    if(survival::is.Surv(y)) ny <- dim(y)[1]
     if(is.null(weights)) weights <- rep.int(1, length(y))
     
     par <- exp(eta)                           
