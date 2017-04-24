37c76a3a102705136d7ab0d50b681e50020e55f4
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2017-04-24 13:43:34 +0000

    require RWeka
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@1274 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/partykit/tests/regtest-party.R
+++ pkg/partykit/tests/regtest-party.R
@@ -10,29 +10,30 @@ pfit <- as.party(fit)
 all(predict(pfit, newdata = kyphosis, type = "node") == fit$where)
 
 ## J48: iris data
-library("RWeka")
-data("iris", package = "datasets")
-itree <- J48(Species ~ ., data = iris)
-pitree <- as.party(itree)
-stopifnot(all(predict(pitree) == predict(pitree, newdata = iris[, 3:4])))
+if (require("RWeka")) {
+    data("iris", package = "datasets")
+    itree <- J48(Species ~ ., data = iris)
+    pitree <- as.party(itree)
+    stopifnot(all(predict(pitree) == predict(pitree, newdata = iris[, 3:4])))
 
-all.equal(predict(itree, type = "prob", newdata = iris),
-          predict(pitree, type = "prob", newdata = iris))
-all.equal(predict(itree,  newdata = iris),
-          predict(pitree, newdata = iris))
+    print(all.equal(predict(itree, type = "prob", newdata = iris),
+              predict(pitree, type = "prob", newdata = iris)))
+    print(all.equal(predict(itree,  newdata = iris),
+              predict(pitree, newdata = iris)))
 
-## rpart/J48: GlaucomaM data
-data("GlaucomaM", package = "TH.data")
-w <- runif(nrow(GlaucomaM))
-fit <- rpart(Class ~ ., data = GlaucomaM, weights = w)
-pfit <- as.party(fit)
-all(predict(pfit, type = "node") == fit$where)
-tmp <- GlaucomaM[sample(1:nrow(GlaucomaM), 100),]
-all.equal(predict(fit, type = "prob", newdata = tmp), predict(pfit, type = "prob", newdata = tmp))
-all.equal(predict(fit, type = "class", newdata = tmp), predict(pfit, newdata = tmp))
-itree <- J48(Class ~ ., data = GlaucomaM)
-pitree <- as.party(itree)
-all.equal(predict(itree, newdata = tmp, type = "prob"), predict(pitree, newdata = tmp, type = "prob"))
+    ## rpart/J48: GlaucomaM data
+    data("GlaucomaM", package = "TH.data")
+    w <- runif(nrow(GlaucomaM))
+    fit <- rpart(Class ~ ., data = GlaucomaM, weights = w)
+    pfit <- as.party(fit)
+    print(all(predict(pfit, type = "node") == fit$where))
+    tmp <- GlaucomaM[sample(1:nrow(GlaucomaM), 100),]
+    print(all.equal(predict(fit, type = "prob", newdata = tmp), predict(pfit, type = "prob", newdata = tmp)))
+    print(all.equal(predict(fit, type = "class", newdata = tmp), predict(pfit, newdata = tmp)))
+    itree <- J48(Class ~ ., data = GlaucomaM)
+    pitree <- as.party(itree)
+    print(all.equal(predict(itree, newdata = tmp, type = "prob"), predict(pitree, newdata = tmp, type = "prob")))
+}
 
 ## rpart: airquality data
 data("airquality")
