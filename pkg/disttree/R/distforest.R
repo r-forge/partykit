afe2f4b752f475a862732e0ffa8789dd1501e3c7
lschlosser (Lisa Schlosser) <Lisa.Schlosser@uibk.ac.at>
2017-05-08 14:41:19 +0000

    the predict method now returns fitted response and fitted parameters separately as set by the input argument 'type'; adjusted format of the output slot 'fitted' and fitted.par' to the corresponding output of disttree objects; bugfixes in predict and logLik method
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@1316 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/disttree/R/distforest.R
+++ pkg/disttree/R/distforest.R
@@ -12,50 +12,78 @@ distforest <- function(formula, data, family = NO(), decorrelate = "none", ntree
   resp.name <- as.character(formula[2])
   
   ## wrapper function to apply distfit in cforest
-  # input: data, family, weights
-  # output: scores (estfun)
-  modelscores_decor <- function(data, weights = NULL) {
+  ytrafo <- function(formula, data, weights = NULL, cluster = cluster, ctrl = control) {
     
-    y <- data[,resp.name]
-    
-    model <- distfit(y, family = family, weights = weights, start = NULL,
-                     vcov = (decorrelate == "vcov"), type.hessian = "analytic", 
-                     estfun = TRUE, cens = cens, censpoint = censpoint, ...)
-    
-    ef <- as.matrix(sandwich::estfun(model))
-    #n <- NROW(ef)
-    #ef <- ef/sqrt(n)
-    
-    if(decorrelate != "none") {
-      n <- NROW(ef)
-      ef <- ef/sqrt(n)
-      
-      vcov <- if(decorrelate == "vcov") {
-        vcov(model) * n
-      } else {
-        solve(crossprod(ef))
-      }
-      
-      root.matrix <- function(X) {
-        if((ncol(X) == 1L)&&(nrow(X) == 1L)) return(sqrt(X)) else {
-          X.eigen <- eigen(X, symmetric = TRUE)
-          if(any(X.eigen$values < 0)) stop("Matrix is not positive semidefinite")
-          sqomega <- sqrt(diag(X.eigen$values))
-          V <- X.eigen$vectors
-          return(V %*% sqomega %*% t(V))
-        }
-      }
-      ef <- as.matrix(t(root.matrix(vcov) %*% t(ef)))
+    if(!(is.null(cluster))) stop("FIX: cluster ignored by trafo-function")
+    if(!(is.numeric(formula[[3]]))) {
+      #print(formula)
+      stop("covariates can only be used as splitting variables (formula has to be of type y~1|x or y~0|x)")
     }
-    return(ef)
-  }
+    
+    decorrelate <- if(is.null(ctrl$decorrelate)) "none" else ctrl$decorrelate  # FIX ME: include in ctrl?
+    
+    modelscores_decor <- function(subset, estfun = TRUE, object = TRUE, info = NULL) {
+      
+      ys <- data[subset,resp.name]
+      subweights <- if(is.null(weights) || (length(weights)==0L)) weights else weights[subset] ## FIX ME: scores with or without weights?
+      # start <- if(!(is.null(info$coefficients))) info$coefficients else NULL
+      start <- info$coefficients
+      
+      model <- distfit(ys, family = family, weights = subweights, start = start,
+                       vcov = (decorrelate == "vcov"), type.hessian = "analytic", 
+                       estfun = estfun, cens = cens, censpoint = censpoint, ...)
+      
+      
+      ef <- as.matrix(sandwich::estfun(model))
+      #n <- NROW(ef)
+      #ef <- ef/sqrt(n)
+      
+      if(decorrelate != "none") {
+        n <- NROW(ef)
+        ef <- ef/sqrt(n)
+        
+        vcov <- if(decorrelate == "vcov") {
+          vcov(model) * n
+        } else {
+          solve(crossprod(ef))
+        }
+        
+        root.matrix <- function(X) {
+          if((ncol(X) == 1L)&&(nrow(X) == 1L)) return(sqrt(X)) else {
+            X.eigen <- eigen(X, symmetric = TRUE)
+            if(any(X.eigen$values < 0)) stop("Matrix is not positive semidefinite")
+            sqomega <- sqrt(diag(X.eigen$values))
+            V <- X.eigen$vectors
+            return(V %*% sqomega %*% t(V))
+          }
+        }
+        ef <- as.matrix(t(root.matrix(vcov) %*% t(ef)))
+      }
+      
+      if(estfun) {
+        estfun <- matrix(0, ncol = ncol(ef), nrow = nrow(data)) 
+        estfun[subset,] <- ef
+      } else estfun <- NULL
+      
+      object <-  if(object) model else NULL
+      
+      ret <- list(estfun = estfun,
+                  coefficients = coef(model, type = "parameter"),
+                  objfun = logLik(model),  # optional function to be maximized (FIX: negative?/minimize?)
+                  object = object,
+                  converged = TRUE  # FIX ME: warnings is distfit does not converge
+      )
+      return(ret)
+    }
+    return(modelscores_decor)
+  }    
   
   
   # rval <- cforest(formula, data, ytrafo = modelscores_decor, ntree = ntree)
   
   ## call cforest
   m <- match.call(expand.dots = FALSE)
-  m$ytrafo <- modelscores_decor
+  m$ytrafo <- ytrafo
   m$control <- control
   # m$data <- data
   m$family <- m$decorrelate <- m$cens <- m$censpoint <- NULL
@@ -93,19 +121,26 @@ distforest <- function(formula, data, family = NO(), decorrelate = "none", ntree
       # logscore[i,] <- pm$familylist$sdist(data[i,resp.name], eta = coef(pm, type = "link"), sum = FALSE)
     }
     
-    names(fitted) <- "(response)"
-    rval$fitted <- fitted
+    if(is.null(weights) || (length(weights)==0L || is.function(weights))) weights <- numeric(nrow(data)) + 1
+    rval$fitted <- as.data.frame(weights, nrow = length(weights))
+    colnames(rval$fitted) <- "(weights)"
+    colnames(fitted) <- "(fitted.response)"
+    rval$fitted$`(response)` <- data[,resp.name]
+    rval$fitted$`(fitted.response)` <- fitted
+    #colnames(rval$fitted) <- c("(weights)", "(response)", "(fitted.response)")
+    
     
     names(fitted.par) <- names(coef(pm, type = "parameter"))
     # names(logscore) <- names(coef(pm, type = "parameter"))
     rval$fitted.par <- fitted.par
     
-    rval$loglik <- loglik
+    rval$loglik <- sum(loglik)
     # rval$logscore <- logscore
   }
   
-  rval$family <- family
-  rval$npar <- np
+  rval$info$family <- family
+  rval$info$npar <- np
+  rval$info$formula <- formula
   
   class(rval) <- c("distforest", class(rval))
   return(rval)
@@ -119,7 +154,7 @@ distforest <- function(formula, data, family = NO(), decorrelate = "none", ntree
 ###################
 # methods for class 'distforest'
 
-predict.distforest <- function (object, newdata = NULL, type = c("response", "prob", 
+predict.distforest <- function (object, newdata = NULL, type = c("response", "parameter", "prob", 
                                                               "weights", "node"), OOB = FALSE, ...) 
 {
   responses <- object$fitted[["(response)"]]
@@ -165,17 +200,43 @@ predict.distforest <- function (object, newdata = NULL, type = c("response", "pr
   
   if(type == "response"){
     if(!is.null(newdata)) {
+
       # get weights for new data
       nw <- predict.cforest(object, newdata = nd, type = "weights", OOB = FALSE)
       
       # calculate prediction for the first observation before the loop in order to get the number of parameters
       resp.name <- as.character(object$info$call$formula[2])
-      pm <-  distfit(object$data[,resp.name], family = object$family, weights = nw[,1], vcov = FALSE)
-      pred.val1 <- predict(pm)
-      pred.par1 <- coef(pm, type = "parameter")
+      pm <-  distfit(object$data[,resp.name], family = object$info$family, weights = nw[,1], vcov = FALSE)
+      pred.val1 <- predict(pm, type = "response")
         
       pred.val <- data.frame(idx = 1:nrow(nd))
       pred.val[1,] <- pred.val1
+      
+      if(nrow(nd)>=2){
+        for(i in 2:nrow(nd)){
+          nwi <- nw[,i]
+          # personalized model
+          pm <-  distfit(object$data[,resp.name], family = object$info$family, weights = nwi, vcov = FALSE)
+          pred.val[i,] <- predict(pm, type = "response")
+        }
+      }
+      pred <- pred.val
+      colnames(pred) <- c("(fitted.response)")
+      return(pred)
+    } else return(object$fitted$`(fitted.response)`)
+  }
+  
+  if(type == "parameter"){
+    if(!is.null(newdata)) {
+      
+      # get weights for new data
+      nw <- predict.cforest(object, newdata = nd, type = "weights", OOB = FALSE)
+      
+      # calculate prediction for the first observation before the loop in order to get the number of parameters
+      resp.name <- as.character(object$info$call$formula[2])
+      pm <-  distfit(object$data[,resp.name], family = object$info$family, weights = nw[,1], vcov = FALSE)
+      pred.par1 <- coef(pm, type = "parameter")
+
       pred.par <- data.frame(matrix(0, nrow = nrow(nd), ncol = length(pred.par1)))
       pred.par[1,] <- pred.par1
       
@@ -183,15 +244,14 @@ predict.distforest <- function (object, newdata = NULL, type = c("response", "pr
         for(i in 2:nrow(nd)){
           nwi <- nw[,i]
           # personalized model
-          pm <-  distfit(object$data[,resp.name], family = object$family, weights = nwi, vcov = FALSE)
-          pred.val[i,] <- predict(pm)
+          pm <-  distfit(object$data[,resp.name], family = object$info$family, weights = nwi, vcov = FALSE)
           pred.par[i,] <- coef(pm, type = "parameter")
         }
       }
-      pred <- cbind(pred.val, pred.par)
-      colnames(pred) <- c("(response)", names(coef(pm, type = "parameter")))
+      pred <- pred.par
+      colnames(pred) <- c(names(coef(pm, type = "parameter")))
       return(pred)
-    } else return(cbind(object$fitted, object$fitted.par))
+    } else return(object$fitted.par)
   }
 }
 
@@ -199,16 +259,17 @@ predict.distforest <- function (object, newdata = NULL, type = c("response", "pr
 
 logLik.distforest <- function(object, newdata = NULL) {
   if(is.null(newdata)) {
-    return(structure(sum(object$loglik), df = object$npar, class = "logLik"))
+    return(structure(object$loglik, df = object$npar, class = "logLik"))
   } else {
     ll <- 0
-    pred.par <- predict(object, newdata = newdata, type = "response")[,-1]
-    np <- ncol(pred.par)
+    pred.par <- predict(object, newdata = newdata, type = "parameter")
+    distlist <- if(inherits(object$info$family, "gamlss.family")) make_dist_list(object$info$family) else object$info$family
     for(i in 1:(nrow(newdata))){
-      par <- pred.par[i,(1:np)]
-      eta <-  as.numeric(object$family$linkfun(par))
-      ll <- ll + object$family$ddist(newdata[i,1], eta = eta,  log=TRUE)
+      par <- pred.par[i,]
+      eta <-  as.numeric(distlist$linkfun(par))
+      ll <- ll + distlist$ddist(newdata[i,paste(object$info$formula[[2]])], eta = eta,  log=TRUE)
     }
+      
     return(ll)
   }
 }
