1bb1dfcd4edf3610b2568af66e9283eb262d0e7f
heidi (Heidi Seibold) <heidi.bold@googlemail.com>
2017-06-02 08:56:55 +0000

    resolve R CMD notes on R-Forge
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@1443 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/model4you/inst/examples/ex-pmodel.R
+++ pkg/model4you/inst/examples/ex-pmodel.R
@@ -27,7 +27,7 @@ if(require("mvtnorm") & require("survival")) {
   set.seed(123)
   beta <- 3
   ntrain <- 500
-  ntest <- 100
+  ntest <- 50
   simdata <- simdata_s <- sim_data(p = 5, beta = beta, n = ntrain)
   tsimdata <- tsimdata_s <- sim_data(p = 5, beta = beta, n = ntest)
   simdata_s$cens <- rep(1, ntrain)
@@ -35,51 +35,34 @@ if(require("mvtnorm") & require("survival")) {
   
   ## base model
   basemodel_lm <- lm(y ~ a, data = simdata)
-  basemodel_wb <- survreg(Surv(y, cens) ~ a, data = simdata_s)
   
   ## forest
   frst_lm <- pmforest(basemodel_lm, ntree = 20, 
                       perturb = list(replace = FALSE, fraction = 0.632),
                       control = ctree_control(mincriterion = 0))
-  frst_wb <- pmforest(basemodel_wb, ntree = 20, 
-                      perturb = list(replace = FALSE, fraction = 0.632),
-                      control = ctree_control(mincriterion = 0))
   
   ## personalised models
+  # (1) return the model objects
   pmodels_lm <- pmodel(x = frst_lm, newdata = tsimdata, fun = identity)
+  class(pmodels_lm)
+  # (2) return coefficients only (default)
   coefs_lm <- pmodel(x = frst_lm, newdata = tsimdata)
   
-  # compare predictive log-Likelihoods of personalised models versus
+  # compare predictive objective functions of personalised models versus
   # base model
-  logLik(pmodels_lm)
-  sum(objfun(basemodel_lm, newdata = tsimdata))
-  
-  coeffun <- function(model) {
-    ## model coefficients
-    coefs <- c(coef(model), scale = model$scale)
-    
-    ## difference in median survival 
-    p = 0.5
-    coefs["median_s0"] <- qweibull(p = p, shape = 1/coefs["scale"], 
-                                   scale = exp(coefs["(Intercept)"]))
-    coefs["median_s1"] <- qweibull(p = p, shape = 1/coefs["scale"], 
-                                   scale = exp(coefs["(Intercept)"] + coefs["aA"]))
-    coefs["median_sdiff"] <- coefs["median_s1"] - coefs["median_s0"]
-    
-    return(coefs)
-  }
-  coefs_wb <- pmodel(x = frst_wb, newdata = tsimdata_s,
-                     fun = coeffun)
+  logLik(pmodels_lm) # -RSS personalised models
+  sum(objfun(basemodel_lm, newdata = tsimdata)) # -RSS base model
   
   
   if(require("ggplot2")) {
     ## dependence plot
     dp_lm <- cbind(coefs_lm, tsimdata)
-    dp_wb <- cbind(coefs_wb, tsimdata)
     ggplot(tsimdata) +
-      stat_function(fun = function(z1) 0.2 + beta * cos(z1), aes(color = "true treatment\neffect")) +
-      geom_point(data = dp_lm, aes(y = aA, x = z1, color = "estimates lm"), alpha = 0.5) +
-      geom_point(data = dp_wb, aes(y = median_sdiff, x = z1, color = "estimates wb"), alpha = 0.5)
+      stat_function(fun = function(z1) 0.2 + beta * cos(z1), 
+                    aes(color = "true treatment\neffect")) +
+      geom_point(data = dp_lm, 
+                 aes(y = aA, x = z1, color = "estimates lm"), 
+                 alpha = 0.5) 
   }
 }
 
