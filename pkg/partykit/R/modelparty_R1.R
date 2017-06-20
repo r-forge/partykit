6218ad7a4ceb4d24ffdf63c052ad8e375d060f65
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2017-06-20 15:37:57 +0000

    pass tp_args to default node_terminal in plot.modelparty
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@1467 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/partykit/R/modelparty_R1.R
+++ pkg/partykit/R/modelparty_R1.R
@@ -1102,7 +1102,7 @@ residuals.modelparty <- function(object, ...)
   return(rval)
 }
 
-plot.modelparty <- function(x, terminal_panel = NULL, FUN = NULL, ...) {
+plot.modelparty <- function(x, terminal_panel = NULL, FUN = NULL, tp_args = NULL, ...) {
   if(is.null(terminal_panel)) {
     if(is.null(FUN)) {
       FUN <- function(x) {
@@ -1112,7 +1112,7 @@ plot.modelparty <- function(x, terminal_panel = NULL, FUN = NULL, ...) {
           strwrap(capture.output(print(cf, digits = 4L))[-1L]))
       }
     }
-    terminal_panel <- node_terminal(x, FUN = FUN)
+    terminal_panel <- do.call("node_terminal", c(list(obj = x, FUN = FUN), tp_args))
   }
   plot.party(x, terminal_panel = terminal_panel, ...)
 }
