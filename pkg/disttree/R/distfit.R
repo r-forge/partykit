11d5fbd388ff450b052e4728684ed92be75d869a
lschlosser (Lisa Schlosser) <Lisa.Schlosser@uibk.ac.at>
2017-07-20 11:02:54 +0000

    calculation of starting values: fixed bug in in condition (& and | instead of && and ||)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@1509 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/disttree/R/distfit.R
+++ pkg/disttree/R/distfit.R
@@ -141,7 +141,8 @@ distfit <- function(y, family, weights = NULL, start = NULL, start.eta = NULL,
   ## calculate initial values if necessary or otherwise transform initial values for the distribution parameters to initial values on the link scale
   if(is.null(start) && is.null(start.eta)){
     starteta <- family$startfun(y, weights = weights)
-    if((starteta[(family$link == "log" | family$link == "logit")] == -Inf) & all(y==0))
+    all0 <- if(is.Surv(y)) all(y[,2]==0) else all(y==0)
+    if((starteta[(family$link == "log" | family$link == "logit")] == -Inf) && all0)
       starteta[(family$link == "log" | family$link == "logit")] <- log(0.0001)
     if(any(is.na(starteta))) {
       if(all(weights == 0)) {
