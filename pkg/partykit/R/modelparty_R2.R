3ccd992239e00da6281956acf810ca8ab5b94523
heidi (Heidi Seibold) <heidi.bold@googlemail.com>
2017-08-21 14:51:12 +0000

    drop regressors without variation from fluctuation tests (see r1517)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@1537 255f4249-2263-4417-b1ea-8498779e27a7

--- pkg/partykit/R/modelparty_R2.R
+++ pkg/partykit/R/modelparty_R2.R
@@ -365,7 +365,7 @@ mob <- function
                               cluster = NULL, control)
 {  
   
-  if(length(unique(z)) < 2L) next
+  if(length(unique(z)) < 2L) return(list(statistic = NA, p.value = NA))
   if(length(weights) == 0) weights <- rep(1, NROW(estfun))
   
   ## set up return values
