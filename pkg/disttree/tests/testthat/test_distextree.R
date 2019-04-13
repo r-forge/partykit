00ceaaca942e390c1b40d3e5fdbc20cd902d5996
mlang (Moritz Lang) <moritz.n.lang@gmail.com>
2019-04-13 21:04:00 +0000

    Perform some basic comparison between old and new tree
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@2532 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: 212b000702c5e0c74a6d35fb148a07dcbb3f2452

--- pkg/disttree/tests/testthat/test_distextree.R
+++ pkg/disttree/tests/testthat/test_distextree.R
@@ -1,5 +1,25 @@
 context("comparison of new implementation 'distextree()' with older version 'disttree()'")
 
+## Check classes
 tr <- distextree(dist ~ speed, data = cars)
 expect_true(all(class(tr) %in% c("modelparty", "party")))
+
+## Compare new with old disttree for tree type equal to 'mob'
+m.old <- disttree(dist ~ speed, data = cars, type.tree = "mob")
+m.new <- distextree(dist ~ speed, data = cars, type.tree = "mob")
+
+expect_equal(coef(m.old), coef(m.new))
+expect_equal(logLik(m.old), logLik(m.new))
+
+## Compare new with old disttree for tree type equal to 'ctree'
+## with old control arguments
+c.old <- disttree(dist ~ speed, data = cars, type.tree = "ctree")
+c.new <- distextree(dist ~ speed, data = cars, type.tree = "ctree", 
+  control = distextree_control(minsplit = 20L, minbucket = 7L, splittry = 2L))
+
+#expect_equal(coef(c.old), coef(c.new)) # FIXME: (ML) mismatches, average diff: 1.89e-06
+expect_equal(coef(c.old), coef(c.new), tolerance = 1e-6)
+#expect_equal(logLik(c.old), logLik(c.new)) # FIXME: (ML) df not return for ctree
+expect_equal(as.numeric(logLik(c.old)), as.numeric(logLik(c.new)))
+
 
