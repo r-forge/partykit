f67712fad50c15b7931f6e68f766cac3fcbb6338
mlang (Moritz Lang) <moritz.n.lang@gmail.com>
2019-07-31 20:38:12 +0000

    Update Description/Namespace/S3methods/Manuals
    - Try to solve warnings and errors of cran check
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@2658 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: 38cd5b98c14b1d6afef9b14e227768ec7aad7a9b

--- pkg/circtree/R/circtree.R
+++ pkg/circtree/R/circtree.R
@@ -3,9 +3,10 @@
 # - AUTHOR: Moritz N. Lang, Lisa Schlosser
 # - DATE:   2019-07-31
 # -------------------------------------------------------------------
-# - PURPOSE: Wrapper function for distextree 
+# - PURPOSE: Wrapper function for distextree plus S3 methods 
 # -------------------------------------------------------------------
 
+## Wrapper function for distextree
 circtree <- function(formula,
                        data,
                        subset,
@@ -13,7 +14,7 @@ circtree <- function(formula,
                        weights,
                        offset,
                        cluster,
-                       control = distextree_control(...),
+                       control = disttree::distextree_control(...),
                        converged = NULL,
                        scores = NULL,
                        doFit = TRUE,
@@ -30,3 +31,47 @@ circtree <- function(formula,
   tree
 }
 
+## Print method
+print.circtree <- function(x, title = NULL, objfun = "negative log-likelihood", ...){
+  if(is.null(title)) title <- sprintf("Circular regression tree (von Mises Distribution)")
+  partykit::print.modelparty(x, title = title, objfun = objfun, ...)
+}
+
+
+## Coef method
+coef.circtree <- function(object, ...){
+  partykit:::coef.modelparty(object)
+}
+
+
+## logLik method
+logLik.circtree <- function(object, newdata = NULL, weights = NULL, ...) {
+  cl <- match.call()
+  cl[[1]] <- quote(disttree:::logLik.distextree)
+  eval(cl)
+}
+
+
+## Predict method
+predict.circtree <- function (object, newdata = NULL, type = c("parameter", "response", "node"), 
+                              OOB = FALSE, ...) {
+  cl <- match.call()
+  cl[[1]] <- quote(disttree:::predict.distextree)
+  eval(cl)
+}
+
+
+## Simulate data
+circtree_simulate <- function(n = 1000, mu = c(0, 2, 1), kappa = c(3, 3, 1), seed = 111){
+  # FIXME: Extend to more general cases
+  set.seed(seed)
+  d <- data.frame(x1 = runif(n, -1, 1), x2 = runif(n, -1, 1))
+  d$group <- ifelse(d$x1 < 0, 1, ifelse(d$x2 < 0, 2, 3))
+  d$mu <- mu[d$group]
+  d$kappa <- kappa[d$group]
+
+  for(i in 1:n){
+    d[i, "y"] <- circular::rvonmises(1, mu = circular::circular(d$mu[i]), kappa = d$kappa[i])
+  }
+  return(d)
+}
