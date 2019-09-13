50f165f2093e1984576037cabcf4bbc1bb71d763
mlang (Moritz Lang) <moritz.n.lang@gmail.com>
2019-09-13 09:32:25 +0000

    Include script to get tawes stations around vie
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@2754 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: bfdb52695de0707b7d71f85c0565d9422f6427f7

---
+++ pkg/circtree/inst/wind_paper/R_scripts/sybase_tawes_around_vie.R
@@ -0,0 +1,37 @@
+# -------------------------------------------------------------------
+# - NAME:        sybase_tawes_around_vie.R
+# - AUTHOR:      Reto Stauffer
+# - DATE:        2018-01-04
+# -------------------------------------------------------------------
+# - DESCRIPTION:
+# -------------------------------------------------------------------
+# - EDITORIAL:   2018-01-04, RS: Created file on pc24-c707.
+# -------------------------------------------------------------------
+# - L@ST MODIFIED: 2019-09-13 on thinkmoritz
+# -------------------------------------------------------------------
+
+library("STAGE")
+
+if (!dir.exists("data")) dir.create("data")
+
+loww <- list(lat = 48.110833, lon = 16.570833)
+stations <- sybasestations("*")
+
+stations$d <- sqrt((stations$lon - loww$lon)^2 + (stations$lat-loww$lat)^2)
+
+stations <- subset(stations,d < .5 & tawes == 1)
+
+params <- c("dd", "ff", "ffx", "tl", "rf", "p", "pred")
+
+for (i in 1:nrow(stations)) {
+  ##
+  outfile <- sprintf("STAGEobs_tawes_wind_%d.rda", stations$statnr[i])
+  if (file.exists(outfile)) next
+  ##
+  obs <- sybase("tawes", stations$statnr[i], parameter = params,
+        begin = "2013-01-01", end = "2019-09-01", archive = TRUE)
+  ##
+  save(file = outfile, obs)
+}
+
+save(file = "data/STAGEtawes_stationlist_extended.rda", stations)
