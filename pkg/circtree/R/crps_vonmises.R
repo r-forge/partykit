85916fc164afdc62db76ece24f6211d500ed8ae8
mlang (Moritz Lang) <moritz.n.lang@gmail.com>
2019-10-01 15:31:21 +0000

    Update plot + crps R-functions, analyses files fit + crps_sim, and man circtree
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@2780 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: c22c11bba922e451b7b4020a82d2f70bf35825e2

--- pkg/circtree/R/crps_vonmises.R
+++ pkg/circtree/R/crps_vonmises.R
@@ -6,21 +6,8 @@
 # - PURPOSE: Circular CRPS (von Mises) based on numeric integration using
 #            the charististic equation
 # -------------------------------------------------------------------
-# - L@ST MODIFIED: 2019-07-31 on thinkmoritz
+# - L@ST MODIFIED: 2019-10-01 on thinkmoritz
 # -------------------------------------------------------------------
-
-### Function
-#crps_vonmises <- function(y, mu, kappa) {
-#
-#  require(CharFun)
-#  
-#  int_fun <- function(x) { 
-#    1 / pi * abs(cfC_vonMises(x, mu, kappa) - exp((0+1i) * x * y))^2 / x^2
-#  } 
-#    
-#  rval <- integrate(int_fun, 0, Inf)$value
-#  return(rval)
-#}
 
 ##YEAR: 2017
 ##COPYRIGHT HOLDER: Ludmila Simkova
@@ -38,9 +25,10 @@ cfC_vonMises <- function(t, mu = 0, kappa = 1){
 
 
 
-## Function
+## CRPS von Mises
 crps_vonmises <- function(y, mu, kappa, sum = FALSE) {
 
+  ## Perform some input checks
   if(any(y < -pi) || any(y > pi) || any(mu < -pi) || any(mu > pi) || any(kappa < 0 ))
     stop("y and mu must be in the interval of [-pi, pi], and kappa must be non negative!") 
 
@@ -49,12 +37,15 @@ crps_vonmises <- function(y, mu, kappa, sum = FALSE) {
     stop("Input 'y', 'mu', and 'kappa' must be numeric vectors...")
   }
 
+  ## Create data.frame (fails if any has not the same length or length equal one)
   dat <- data.frame("y" = y, "mu" = mu, "kappa" = kappa)
 
-  idx <- apply(abs(matrix(dat$mu, ncol = 3, nrow = nrow(dat), byrow = FALSE) 
-    + c(-2 * pi, 0, 2 * pi) - dat$y), 1, which.min)
+  ## Get mu with smallest absolute distance
+  idx <- apply(abs(matrix(dat$mu, ncol = 3, nrow = nrow(dat), byrow = FALSE) +
+    matrix(c(-2 * pi, 0, 2 * pi), ncol = 3, nrow = nrow(dat), byrow = TRUE) - dat$y), 1, which.min)
   dat$mu <- dat$mu + c(-2 * pi, 0, 2 * pi)[idx]
-  
+
+  ## Calculate CRPS based on characteristic function  
   rval <- sapply(1:nrow(dat), function(i){
 
     int_fun <- function(x) {
@@ -69,54 +60,3 @@ crps_vonmises <- function(y, mu, kappa, sum = FALSE) {
   if(sum) rval <- sum(rval)
   return(rval)
 }
-
-### Function
-#crps_vonmises_new <- function(y, mu, kappa, sum = FALSE) {
-#
-#  if(any(y < -pi) || any(y > pi) || any(mu < -pi) || any(mu > pi) || any(kappa < 0 ))
-#    stop("y and mu must be in the interval of [-pi, pi], and kappa must be non negative!") 
-#
-#  require(CharFun)
-#
-#  if(!inherits(y, c("numeric", "integer")) || !inherits(mu, c("numeric", "integer")) ||
-#    !inherits(kappa, c("numeric", "integer"))) {
-#    stop("Input 'y', 'mu', and 'kappa' must be numeric vectors...")
-#  }
-#
-#  dat <- data.frame("y" = y, "mu" = mu, "kappa" = kappa)
-#
-#  idx <- apply(abs(matrix(dat$mu, ncol = 3, nrow = nrow(dat), byrow = FALSE) 
-#    + c(-2 * pi, 0, 2 * pi) - dat$y), 1, which.min)
-#  dat$mu <- dat$mu + c(-2 * pi, 0, 2 * pi)[idx]
-#  
-#  rval <- sapply(1:nrow(dat), function(i){
-#
-#    int_fun <- function(x) {
-#      (circular::pvonmises(x, dat[i, "mu"], dat[i, "kappa"]) - as.numeric(dat[i, "y"] <= x))
-#    }
-#
-#    crps <- integrate(int_fun, -pi, pi)$value
-#    #browser(); curve(int_fun, from = -100, to = 100)
-#
-#    return(crps)
-#  })
-#
-#  if(sum) rval <- sum(rval)
-#  return(rval)
-#}
-
-
-### Testing
-#grid <- expand.grid(mu = seq(0, 2 * pi, by = pi / 16), kappa = seq(0.1,10, by = 0.2))
-#
-#for(i in 1:nrow(grid)){
-#  grid$crps[i] <- crps_vonmises(pi/2, mu = grid[i, "mu"], kappa = grid[i, "kappa"])
-#}
-##bamlss::plot3d(grid, image = TRUE)
-#mgrid <- matrix(grid$crps, nrow = 33)
-#image(unique(grid$mu), unique(grid$kapp), mgrid, col = colorspace::sequential_hcl(31, "Oslo"),
-#  xlab = "mu [rad]", ylab = "kappa")
-#
-#test <- crps_vonmises(rep(pi/2, nrow(grid)), mu = grid[, "mu"], kappa = grid[,"kappa"])
-
-
