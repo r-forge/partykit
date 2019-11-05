55d28b251c553e2264646c295b9e67bfaa688440
lschlosser (Lisa Schlosser) <Lisa.Schlosser@uibk.ac.at>
2019-11-05 15:21:41 +0000

    basic version of trafo and variable selection function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@2818 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: 72523924ea66216cd38f4eac9ffe849ea4a85808

---
+++ pkg/extree_partykit/inst/19_extree_hackathon/selection_modules.R
@@ -0,0 +1,106 @@
+trafo <- function(data){
+  estfun <- data$yx$y
+  return(estfun)
+}
+
+
+
+# variable selection for numeric splitvariable with index j
+var_select_num <- function(estfun, data, j){
+  
+  # categorize estfun if not already a factor
+  est_cat <- if(is.factor(estfun)) estfun else cut(estfun, breaks = quantile(estfun, c(0, 0.5, 1)), 
+                                                   include.lowest = TRUE, right = TRUE)
+  
+  # get possible split variable
+  sv <- data$zindex[[j]]
+  
+  # categorize possible split variable
+  sv_cat <- cut(sv, breaks = quantile(sv, c(0,0.25, 0.5, 0.75,1)), 
+                include.lowest = TRUE, right = TRUE)
+  
+  # independence test 
+  test <- chisq.test(x = est_cat, y = sv_cat)
+  res <- c(test$p.value, test$statistic)
+  names(res) <- c("p.value", "statistic")
+
+  return(res)
+}
+
+
+# variable selection for categorical splitvariable with index j
+var_select_cat <- function(estfun, data, j){
+  
+  # categorize estfun if not already a factor
+  est_cat <- if(is.factor(estfun)) estfun else cut(estfun, breaks = quantile(estfun, c(0, 0.5, 1)), 
+                                                   include.lowest = TRUE, right = TRUE)
+  
+  # get possible split variable
+  sv_cat <- data$zindex[[j]]
+  
+  # independence test
+  test <- chisq.test(x = est_cat, y = sv_cat)
+  res <- c(test$p.value, test$statistic)
+  names(res) <- c("p.value", "statistic")
+  
+  return(res)
+}
+
+
+
+# unifying function for variable selection using var_select_cat or var_select_num
+var_select <- function(estfun, data, j){
+  if(class(data[[j]]) == "factor"){
+    res <- var_select_cat(estfun, data, j)
+  }
+  if(class(data[[j]]) == "numeric"){
+    res <- var_select_num(estfun, data, j)
+  }
+  return(res)
+}
+
+# general function for complete data set and all types of splitvariables
+var_select_all <- function(estfun, data){
+  
+  # categorize estfun if not already a factor
+  est_cat <- if(is.factor(estfun)) estfun else cut(estfun, breaks = quantile(estfun, c(0, 0.5, 1)), 
+                                                    include.lowest = TRUE, right = TRUE)
+  
+  # select possible splitvariables
+  is.splitvar <- !sapply(data$zindex, FUN = is.null)
+  
+  # store p-value and test statistic in matrix 'res'
+  res <- matrix(nrow = sum(is.splitvar), ncol = 2)
+  colnames(res) <- c("p.value", "statistic")
+  rownames(res) <- names(is.splitvar[is.splitvar])
+    
+  # independence test over all possible splitvariables
+  for(j in 1:length(is.splitvar)){
+    if(is.splitvar[j]){
+      sv <- data$zindex[[j]]
+      # categorize possible splitvariables
+      sv_cat <- if(is.factor(sv)) sv else cut(sv, breaks = quantile(sv, c(0,0.25, 0.5, 0.75,1)), 
+                                              include.lowest = TRUE, right = TRUE)
+      test <- chisq.test(x = est_cat, y = sv_cat)
+      res[names(is.splitvar[j]),"p.value"] <- test$p.value
+      res[names(is.splitvar[j]),"statistic"] <- test$statistic
+    }
+  }
+  
+  return(res)
+}
+
+
+
+
+if(FALSE){
+  
+  library("partykit")
+  
+  d <-  extree_data(Species ~ Sepal.Width + Sepal.Length | Petal.Width + Petal.Length,
+                    data = iris, yx = "matrix")
+  
+  ef <- trafo(d)
+  varsel <- var_select(ef, d, 4)
+
+}
