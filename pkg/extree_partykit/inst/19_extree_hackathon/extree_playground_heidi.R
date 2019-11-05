fa43db0bf3191b98a16137fa5bcfc7c5a01f9779
heidi (Heidi Seibold) <heidi.bold@googlemail.com>
2019-11-05 15:08:16 +0000

    some code for trying stuff with extree
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@2817 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: 1bf86ec8727fc950936d3079e1a6cb66bc1fb74e

---
+++ pkg/extree_partykit/inst/19_extree_hackathon/extree_playground_heidi.R
@@ -0,0 +1,43 @@
+library("partykit")
+
+airq <- subset(airquality, !is.na(Ozone))
+my_data <- extree_data(Ozone ~ Wind + Temp, 
+    data = airq, yx = "matrix")
+
+## This currently needs to be done within extree
+# fitter <- function(subset, data, weights, info = NULL, estfun = TRUE, object = TRUE) {
+#     estfun <- matrix(0, ncol = NCOL(data$yx$y), nrow = nrow(data$data))
+#     estfun[subset,] <- as.matrix(data$yx$y)[subset, ]
+#     list(estfun = estfun, converged = TRUE)
+# }
+
+my_split <- function(...) {
+    msfn <- function(model, trafo, data, subset, weights, whichvar, ctrl) {
+        # args <- list(...)
+        
+        if (length(whichvar) == 0) return(NULL)
+        
+        ## split LAST variable at median
+        for (j in whichvar) {
+            x <- model.frame(data)[[j]][subset]
+            ret <- partysplit(as.integer(j), breaks = median(x))
+        }
+        return(ret)
+    }
+    
+    return(msfn)
+}
+
+
+# debug(my_split)
+# undebug(partykit:::.extree_node)
+
+tr <- extree(data = my_data, #trafo = fitter, 
+    control = c(extree_control(criterion = "statistic",
+        logmincriterion = log(1 - 0.04),
+        update = TRUE,
+        selectfun = partykit:::.objfun_select(),
+        splitfun = my_split(),
+        svselectfun = partykit:::.objfun_select(),
+        svsplitfun = my_split()),
+        restart = TRUE))
