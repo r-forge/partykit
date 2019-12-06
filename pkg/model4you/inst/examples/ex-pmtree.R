b4e243b114d2aca6e1a36d62a56f6ef8156f246f
heidi (Heidi Seibold) <heidi.bold@googlemail.com>
2019-12-06 12:37:11 +0000

    class()== -> is()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@2918 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: efb6333432d8ef9dc57c86e4be727afd9e9a65a5

--- pkg/model4you/inst/examples/ex-pmtree.R
+++ pkg/model4you/inst/examples/ex-pmtree.R
@@ -12,4 +12,20 @@ if(require("TH.data") & require("survival")) {
   
   logLik(bmod)
   logLik(tr)
+  
+  
+  ## Sometimes the number of participant in each treatment group needs to 
+  ## be of a certain size. This can be accomplished using converged
+  
+  ## Each treatment group should have more than 33 observations
+  ctrl <- ctree_control(lookahead = TRUE)
+  ctrl$converged <- function(mod, data, subset) {
+      all(table(data$horTh[subset]) > 33)
+  }
+  
+  tr2 <- pmtree(bmod, control = ctrl)
+  plot(tr2, terminal_panel = node_pmterminal(tr, plotfun = survreg_plot,
+      confint = TRUE))
+  
+  summary(tr2[[5]]$data$horTh)
 }
