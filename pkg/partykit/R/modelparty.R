2db7121b2ce87c762fe61e99d20350d4f859f877
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2020-07-21 22:34:07 +0000

    assure that the sequence from:to in fluctests is actually non-empty for caseweighted process
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@3065 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: cb137812a5a3ed2ea8b6b418cb901564920f07b2

--- pkg/partykit/R/modelparty.R
+++ pkg/partykit/R/modelparty.R
@@ -352,15 +352,16 @@ mob_partynode <- function(Y, X, Z, weights = NULL, offset = NULL, cluster = NULL
         proci <- process[oi, , drop = FALSE]
         proci <- apply(proci, 2L, cumsum)
 	tt0 <- if(control$caseweights && any(weights != 1L)) cumsum(weights[oi]) else 1:n
-        stat[i] <- if(from < to) {
+	from_to <- tt0 >= from & tt0 <= to
+        stat[i] <- if(length(from_to) > 0L) {
 	  xx <- rowSums(proci^2)
-	  xx <- xx[tt0 >= from & tt0 <= to]
-	  tt <- tt0[tt0 >= from & tt0 <= to]/nobs
+	  xx <- xx[from_to]
+	  tt <- tt0[from_to]/nobs
 	  max(xx/(tt * (1 - tt)))	  
 	} else {
 	  0
 	}
-        pval[i] <- if(from < to) logp.supLM(stat[i], k, lambda) else NA
+        pval[i] <- if(length(from_to) > 0L) logp.supLM(stat[i], k, lambda) else NA
       }
     }
 
