d70ca9370e6f3aa219253d610ec4f8c700e8ecf1
mlang (Moritz Lang) <moritz.n.lang@gmail.com>
2020-11-27 10:38:00 +0000

    Run test for comparison with older versions only at home
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@3127 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: 3d9e85baf10937ff4dc55fa16cc355deb1ffc726

--- pkg/disttree/tests/tinytest.R
+++ pkg/disttree/tests/tinytest.R
@@ -1,5 +1,6 @@
 ## Trigger tiny tests within /inst/tinytest/
 
 if(requireNamespace("tinytest", quietly=TRUE)){
+##  home <- identical(Sys.getenv("TT_AT_HOME"), "TRUE")  ## To trigger all tests only at home.
   tinytest::test_package("disttree")
 }
