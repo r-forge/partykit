15addbd6e2d9b4f0a144be370bfa12f58742b239
marjolein_f (Marjolein Fokkema) <marjolein.fokkema@gmail.com>
2023-09-07 23:03:39 +0000

    version 2.0-4
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@3272 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: 493436c536f115cc0b2ae6e9ffeb0e9b115f8fad

--- pkg/glmertree/tests/test_glmertree.R
+++ pkg/glmertree/tests/test_glmertree.R
@@ -6,22 +6,25 @@ lmm_tree <- lmertree(depression ~ treatment | cluster |
                        age + duration + anxiety, data = DepressionDemo)
 plot(lmm_tree, which = "tree")
 plot(lmm_tree, which = "ranef")
-print(lmm_tree)
-coef(lmm_tree)
-ranef(lmm_tree)
-predict(lmm_tree, newdata = DepressionDemo[1:7, ])
-predict(lmm_tree, newdata = DepressionDemo[1:7, -3], re.form = NA)
-residuals(lmm_tree)[1:10]
-predict(lmm_tree)[1:10]
+round(coef(lmm_tree), digits = 6)
+round(ranef(lmm_tree)$cluster, digits = 6)
+formatC(predict(lmm_tree, newdata = DepressionDemo[1:7, ]), format = "f", 
+        digits = 7)
+formatC(predict(lmm_tree, newdata = DepressionDemo[1:7, -3], re.form = NA),
+        format = "f", digits = 7)
+formatC(residuals(lmm_tree)[1:10], format = "f", digits = 7)
+formatC(predict(lmm_tree)[1:10], format = "f", digits = 7)
+
 glmm_tree <- glmertree(depression_bin ~ treatment | 
                          cluster | age + duration + anxiety, data = DepressionDemo, 
                        family = binomial)
 plot(glmm_tree, which = "tree")
 plot(glmm_tree, which = "ranef")
-print(glmm_tree)
-coef(glmm_tree)
-ranef(glmm_tree)
-predict(glmm_tree, newdata = DepressionDemo[1:7, ])
-predict(glmm_tree, newdata = DepressionDemo[1:7, -3], re.form = NA)
-residuals(glmm_tree)[1:10]
-predict(glmm_tree)[1:10]
+round(coef(glmm_tree), digits = 6)
+round(ranef(glmm_tree)$cluster, digits = 6)
+formatC(predict(glmm_tree, newdata = DepressionDemo[1:7, ]), format = "f", 
+        digits = 7)
+formatC(predict(glmm_tree, newdata = DepressionDemo[1:7, -3], re.form = NA), 
+        format = "f", digits = 7)
+formatC(residuals(glmm_tree)[1:10], format = "f", digits = 7)
+formatC(predict(glmm_tree)[1:10], format = "f", digits = 7)
