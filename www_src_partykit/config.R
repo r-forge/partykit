4c4537e384f239c3db4bb7787157a8c1bfb4365b
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2024-07-18 12:38:13 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@3288 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: 072b864de8b770cdaa8f5378c90dbfff611dbb2d

--- www_src_partykit/config.R
+++ www_src_partykit/config.R
@@ -3,5 +3,5 @@ pkg <- "partykit"
 dest <- "html"
 publish <- "../www/partykit"
 
-download.file("http://user.math.uzh.ch/hothorn/TH.bib", dest = "TH.bib")
+download.file("http://user.math.uzh.ch/hothorn/bib/TH.bib", dest = "TH.bib")
 bib <- "TH.bib"
