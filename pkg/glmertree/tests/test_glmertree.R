00adf2ecaec76da9ad4c887136ae17e0e2442872
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2024-09-19 11:22:18 +0000

    Reduce precision in tests in order to avoid NOTEs in CRAN checks.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@3295 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: d303c638c6ecf20d395057018cb8f30ab1b990a7

--- pkg/glmertree/tests/test_glmertree.R
+++ pkg/glmertree/tests/test_glmertree.R
@@ -1,30 +1,27 @@
 library("glmertree")
-options(width = 70, prompt = "R> ", continue = "+  ")
+options(width = 70, prompt = "R> ", continue = "+  ", digits = 4)
+
 data("DepressionDemo", package = "glmertree")
 summary(DepressionDemo)
-lmm_tree <- lmertree(depression ~ treatment | cluster | 
-                       age + duration + anxiety, data = DepressionDemo)
+
+lmm_tree <- lmertree(depression ~ treatment | cluster | age + duration + anxiety,
+  data = DepressionDemo)
 plot(lmm_tree, which = "tree")
 plot(lmm_tree, which = "ranef")
-round(coef(lmm_tree), digits = 6)
-round(ranef(lmm_tree)$cluster, digits = 6)
-formatC(predict(lmm_tree, newdata = DepressionDemo[1:7, ]), format = "f", 
-        digits = 7)
-formatC(predict(lmm_tree, newdata = DepressionDemo[1:7, -3], re.form = NA),
-        format = "f", digits = 7)
-formatC(residuals(lmm_tree)[1:10], format = "f", digits = 7)
-formatC(predict(lmm_tree)[1:10], format = "f", digits = 7)
+coef(lmm_tree)
+ranef(lmm_tree)$cluster
+predict(lmm_tree, newdata = DepressionDemo[1:7, ])
+predict(lmm_tree, newdata = DepressionDemo[1:7, -3], re.form = NA)
+residuals(lmm_tree)[1:10]
+predict(lmm_tree)[1:10]
 
-glmm_tree <- glmertree(depression_bin ~ treatment | 
-                         cluster | age + duration + anxiety, data = DepressionDemo, 
-                       family = binomial)
+glmm_tree <- glmertree(depression_bin ~ treatment | cluster | age + duration + anxiety,
+  data = DepressionDemo, family = binomial)
 plot(glmm_tree, which = "tree")
 plot(glmm_tree, which = "ranef")
-round(coef(glmm_tree), digits = 6)
-round(ranef(glmm_tree)$cluster, digits = 6)
-formatC(predict(glmm_tree, newdata = DepressionDemo[1:7, ]), format = "f", 
-        digits = 7)
-formatC(predict(glmm_tree, newdata = DepressionDemo[1:7, -3], re.form = NA), 
-        format = "f", digits = 7)
-formatC(residuals(glmm_tree)[1:10], format = "f", digits = 7)
-formatC(predict(glmm_tree)[1:10], format = "f", digits = 7)
+coef(glmm_tree)
+ranef(glmm_tree)$cluster
+predict(glmm_tree, newdata = DepressionDemo[1:7, ])
+predict(glmm_tree, newdata = DepressionDemo[1:7, -3], re.form = NA)
+residuals(glmm_tree)[1:10]
+predict(glmm_tree)[1:10]
