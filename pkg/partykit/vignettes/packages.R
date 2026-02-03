410860994204cb199e4674aed95634ea78253cd5
thothorn (Torsten Hothorn) <Torsten.Hothorn@R-project.org>
2026-02-03 12:59:05 +0000

    cite packages correctly
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/partykit@3325 255f4249-2263-4417-b1ea-8498779e27a7
    
    Former-commit-id: 50089b82457065a490ecf4d7a53afda3eb3f7401

---
+++ pkg/partykit/vignettes/packages.R
@@ -0,0 +1,37 @@
+
+if (file.exists("packages.bib")) file.remove("packages.bib")
+pkgversion <- function(pkg) {
+    pkgbib(pkg)
+    packageDescription(pkg)$Version
+}
+pkgbib <- function(pkg) {
+    x <- citation(package = pkg, auto = TRUE)[[1]]
+    b <- toBibtex(x)
+    b <- gsub("Buehlmann", "B{\\\\\"u}hlmann", b)
+    b[1] <- paste("@Manual{pkg:", pkg, ",", sep = "")
+#    if (is.na(b["url"])) {
+#        b[length(b)] <- paste("   URL = {http://CRAN.R-project.org/package=",
+#                              pkg, "},", sep = "")
+#    }
+    b <- b[names(b) != "url"]
+    if (is.na(b["doi"])) {
+        b[length(b)] <- paste("   DOI = {10.32614/CRAN.package.",
+                              pkg, "}", sep = "")
+        b <- c(b, "}")
+    }
+    if (length(grep("commit", b["note"])) > 0)
+        b["note"] <- paste0(gsub(", commit.*", "", b["note"]), "},")
+    cat(b, sep = "\n", file = "packages.bib", append = TRUE)
+}
+pkg <- function(pkg)
+    paste("\\\\pkg{", pkg, "} \\\\citep[version~",
+          pkgversion(pkg), ",][]{pkg:", pkg, "}", sep = "")
+
+
+pkgs <- c("mvtnorm", "partykit", "party", "psychotree", "C50", # "mvpart",
+"survival", "rpart", "modeltools", "mlbench", "pmml", "RWeka")
+p <- sapply(pkgs, require, character.only = TRUE)
+if (!all(p))
+    try(install.packages(pkgs[!p], repos = "https://CRAN.R-project.org"))
+
+sapply(pkgs, pkg)
